b6d3a92d275046265f88360545df8ac601e8779f
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-10-19 15:38:46 -0300

    Implementando a classe base item

---
+++ src/main/java/com/dio/api/bank/dominio/model/Noticias.java
@@ -0,0 +1,7 @@
+package com.dio.api.bank.dominio.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_noticias")
+public class Noticias extends BaseItem{
+}
---
+++ src/main/java/com/dio/api/bank/dominio/model/Recurso.java
@@ -0,0 +1,7 @@
+package com.dio.api.bank.dominio.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_recursos")
+public class Recurso extends BaseItem{
+}
