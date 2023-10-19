88998b47c1ba65c7933bc558e1a4c879d9f3ea4c
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-10-19 12:15:49 -0300

    Initial commit

---
+++ src/test/java/com/dio/api/bank/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.api.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
