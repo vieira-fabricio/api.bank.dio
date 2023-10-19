c4a3121c3b0fa03b9a3dd06a930d3b43938636af
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-10-19 15:38:14 -0300

    Implementando a classe cartão

---
+++ src/main/java/com/dio/api/bank/dominio/model/Cartao.java
@@ -0,0 +1,40 @@
+package com.dio.api.bank.dominio.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_cartao")
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String numero;
+    @Column(precision = 13, scale = 2)
+    private BigDecimal limite;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
+}
